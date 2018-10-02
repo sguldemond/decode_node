f759503313022b9cc7740a76e9ff3f61c0df0ae3
stan-is-laus <stanguldemond@gmail.com>
2018-10-02 13:18:48 +0200

    development

--- index.js
+++ index.js
@@ -6,19 +6,18 @@ const terminalImage = require('terminal-image');
 const DECODE_SERVER_BASE = "https://decode.stadswerken.amsterdam/";
 const DECODE_WEB_BASE = "http://vegter.github.io/decode"
 
-// startSession();
 
-initDisclosure()
-  .then(getSessionId)
-  .then(generateQR)
-  .then(getSessionStatus)
-  .catch(err => {
-    console.error(err);
-  });
+startSession();
 
-// (async () => {
-//   console.log(await terminalImage.file('./files/default-man.png'));
-// })();
+function startSession(){
+  initDisclosure()
+    .then(getSessionId)
+    .then(generateQR)
+    .then(getSessionStatus)
+    .catch(err => {
+      console.error(err);
+    });
+}
 
 function initDisclosure() {
   let attribute_request = "ouderdan18";
@@ -62,10 +61,13 @@ function getSessionData(sessionId) {
   console.log(session_id);
   got.post(DECODE_SERVER_BASE, { path: "/get_session", json: true, body: { session_id } })
     .then(res => {
-      console.log(res.body);      
+      // console.log(res.body);
+      (async () => {
+        console.log(await terminalImage.file('./files/default-man.png'));
+      })();
     }).catch(err => {
       console.error(err)
     });
   
-  // setTimeout(startSession, 3000);
+  setTimeout(startSession, 3000);
 }
